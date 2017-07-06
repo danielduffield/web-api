b00d455ee2ed0b1bad74963eda9dafd9b2b035d4
Daniel Duffield <danieljduffield@gmail.com>
2017-07-06 09:25:11 -0700

    Added JSON body parser to app use middleware to convert request body into JSON. #1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/* eslint-disable no-unused-vars */
-
 const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 const express = require('express')
 const app = express()
+
+app.use(jsonParser)
 
 app.listen(3000, () => console.log('Listening on 3000.'))
