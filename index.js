f75f8f80e2d5236960fd906d9ed179967ea4c871
Daniel Duffield <danieljduffield@gmail.com>
2017-07-06 09:33:13 -0700

    Added a handler for POST requests that adds the req body to an array and then sets the id equal to the array index + 1. #1

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const jsonParser = bodyParser.json()
 const express = require('express')
 const app = express()
 
+const notes = []
+
 app.use(jsonParser)
+
+app.post('/notes/', (req, res) => {
+  notes.push(req.body)
+  notes.forEach((note, index) => {
+    note.id = index
+  })
+  res.sendStatus(201)
+})
 
 app.listen(3000, () => console.log('Listening on 3000.'))
